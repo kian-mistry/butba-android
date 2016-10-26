33ef69c47d712bc2dd82fb7dbeced8b9297b0503
Kian Mistry <kian.mistry@gmail.com>
2016-10-27 00:27:12 +0100

    Added a ListPreference which will hold the name of each BUTBA member.

--- app/src/main/java/com/kian/butba/settings/SettingsActivity.java
+++ app/src/main/java/com/kian/butba/settings/SettingsActivity.java
@@ -1,11 +1,17 @@
 package com.kian.butba.settings;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.kian.butba.R;
+
+import java.util.Arrays;
 
 /**
  * Created by Kian Mistry on 26/10/16.
@@ -37,10 +43,56 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class SettingsFragment extends PreferenceFragment {
 
+        private ListPreference listButbaMembers;
+
+        private CharSequence[] entries = {
+                "Kian Mistry",
+                "Sarah Hood",
+                "Paul Marks"
+        };
+
+        CharSequence[] entryValues = {
+                "41", "2", "5"
+        };
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_settings);
+
+            //Set up list preference.
+            listButbaMembers = (ListPreference) findPreference("pref_butba_member");
+            setListPreference(listButbaMembers, entries, entryValues);
+            listButbaMembers.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    setListPreference(listButbaMembers, entries, entryValues);
+                    return false;
+                }
+            });
+
+            listButbaMembers.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                    /* Obtains the index of the new value which corresponds to the elements in the
+                     * entryValues variable.
+                     *
+                     * The position of each element in the entryValues variable, corresponds to each
+                     * position of each element in the entries variable.
+                     */
+                    int position = Arrays.asList(entryValues).indexOf(newValue);
+                    preference.setSummary(entries[position]);
+
+                    return true;
+                }
+            });
+
+        }
+
+        private void setListPreference(ListPreference listPreference, CharSequence[] entries, CharSequence[] entryValues) {
+            listPreference.setEntries(entries);
+            listPreference.setEntryValues(entryValues);
         }
     }
 }
