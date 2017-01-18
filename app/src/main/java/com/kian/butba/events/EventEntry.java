e2fb309b4a2ad3cd758869028085a8aa5469d39f
Kian Mistry <kian.mistry@gmail.com>
2017-01-18 19:21:06 +0000

    The entry form is able to ask the user for a specific number of bowler's and their averages based on the team size of each tournament.

--- app/src/main/java/com/kian/butba/events/EventEntry.java
+++ app/src/main/java/com/kian/butba/events/EventEntry.java
@@ -2,13 +2,22 @@ package com.kian.butba.events;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.kian.butba.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Kian Mistry on 17/01/17.
@@ -20,6 +29,12 @@ public class EventEntry extends AppCompatActivity {
 
 	private String eventName;
 	private int eventTeamSize;
+
+	private TextView eventEntryName;
+	private GridLayout eventEntryAveragesGrid;
+
+	private ArrayList<HashMap<String, String>> bowlersEditTextIds;
+	private ArrayList<HashMap<String, Integer>> bowlersAverages;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +53,65 @@ public class EventEntry extends AppCompatActivity {
 
 		eventName = intent.getStringExtra("EVENT_NAME");
 		eventTeamSize = intent.getIntExtra("EVENT_TEAM_SIZE", 0);
-		Toast.makeText(this, eventName + " " + eventTeamSize, Toast.LENGTH_SHORT).show();
+
+		//Initialise views.
+		eventEntryName = (TextView) findViewById(R.id.event_entry_name);
+		eventEntryName.setText(eventName);
+
+		eventEntryAveragesGrid = (GridLayout) findViewById(R.id.event_entry_averages_grid);
+		setupAveragesView(eventTeamSize);
+	}
+
+	/**
+	 * Populate the entry form with fields asking for the bowler's name and average depending on the team size for the event.
+	 *
+	 * @param teamSize The size of each team for the event.
+	 */
+	private void setupAveragesView(int teamSize) {
+		if(teamSize > 0) {
+			for(int i = 0; i < teamSize; i++) {
+				//Create a linear layout for each row of the grid layout.
+				LinearLayout linearLayout = new LinearLayout(this);
+				linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+				linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+				//Create text input layout.
+				TextInputLayout textInputLayoutBowler = new TextInputLayout(this);
+				textInputLayoutBowler.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 3));
+
+				//Create edit text for bowler's name.
+				//TODO: Find a way to generate unique IDs for views.
+				EditText editTextBowler = new EditText(this);
+				editTextBowler.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+				editTextBowler.setInputType(InputType.TYPE_CLASS_TEXT);
+				editTextBowler.setHint("Bowler " + (i + 1));
+
+				//Add to text input layout.
+				textInputLayoutBowler.addView(editTextBowler);
+
+				//Add to linear layout.
+				linearLayout.addView(textInputLayoutBowler);
+
+				//Create text input layout.
+				TextInputLayout textInputLayoutBowlerAverage = new TextInputLayout(this);
+				textInputLayoutBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
+
+				//Create edit text for bowler's name.
+				EditText editTextBowlerAverage = new EditText(this);
+				editTextBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+				editTextBowlerAverage.setInputType(InputType.TYPE_CLASS_NUMBER);
+				editTextBowlerAverage.setHint("Average");
+
+				//Add to text input layout.
+				textInputLayoutBowlerAverage.addView(editTextBowlerAverage);
+
+				//Add to linear layout.
+				linearLayout.addView(textInputLayoutBowlerAverage);
+
+				//Add linear layout to grid layout.
+				eventEntryAveragesGrid.addView(linearLayout);
+			}
+		}
 	}
 
 	@Override
