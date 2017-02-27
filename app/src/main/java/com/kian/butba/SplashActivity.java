ece5b9ac5a2a204e5ea2e7dee629f9c5a71b6f31
Kian Mistry <kian.mistry@gmail.com>
2017-02-27 23:42:10 +0000

    Checks for an Internet connection on app initialisation.

--- app/src/main/java/com/kian/butba/SplashActivity.java
+++ app/src/main/java/com/kian/butba/SplashActivity.java
@@ -4,11 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.kian.butba.database.sqlite.DatabaseOperations;
+import com.kian.butba.file.FileOperations;
 
 public class SplashActivity extends Activity {
+
+	private SharedPreferences prefInitialisation;
+	private boolean isInitialised = false;
+
+	private boolean isConnected = false;
 
 	private SharedPreferences prefDatabase;
 	private boolean tableAcademicYearExists;
@@ -19,13 +27,22 @@ public class SplashActivity extends Activity {
 	private boolean tableStudentStatusExists;
 	private boolean tableUniversityExists;
 
+	private TextView tvStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-	    //Obtain shared preferences.
+	    //Initialise views.
+	    tvStatus = (TextView) findViewById(R.id.splash_status);
+
+	    //Check whether app has been ran for the first time.
+	    prefInitialisation = getSharedPreferences("butba_initialisation", Context.MODE_PRIVATE);
+	    isInitialised = prefInitialisation.getBoolean("pref_initialised", false);
+
+	    //Check whether tables have been downloaded.
 	    prefDatabase = getSharedPreferences("butba_database", Context.MODE_PRIVATE);
 	    tableAcademicYearExists = prefDatabase.getBoolean("pref_table_academic_year", false);
 	    tableBowlerExists = prefDatabase.getBoolean("pref_table_bowlers", false);
@@ -35,49 +52,79 @@ public class SplashActivity extends Activity {
 	    tableStudentStatusExists = prefDatabase.getBoolean("pref_table_student_status", false);
 	    tableUniversityExists = prefDatabase.getBoolean("pref_table_university", false);
 
-        //Wait for x seconds, until the home screen is shown.
-        Thread timer = new Thread() {
+	    //Wait for x seconds, until the home screen is shown.
+	    Thread timer = new Thread() {
 
-            @Override
-            public void run() {
-                try {
-	                //If all data has been downloaded, show the splash screen for 1.5 seconds.
-	                if(
-			                tableAcademicYearExists &&
-			                tableBowlerExists &&
-			                tableBowlerSeasonsExists &&
-			                tableEventCodesExists &&
-			                tableRankingStatusExists &&
-			                tableStudentStatusExists &&
-			                tableUniversityExists
-	                ) {
-		                sleep(1500);
-	                }
-	                else {
-		                //Stay on splash screen until all the data has been downloaded.
-		                prefCheckTablesExists();
+		    @Override
+		    public void run() {
+			    try {
+				    //If all data has been downloaded, show the splash screen for 1.5 seconds.
+				    if(
+						    !tableAcademicYearExists ||
+							!tableBowlerExists ||
+							!tableBowlerSeasonsExists ||
+							!tableEventCodesExists ||
+							!tableRankingStatusExists ||
+							!tableStudentStatusExists ||
+							!tableUniversityExists
+					) {
+						//Stay on splash screen until all the data has been downloaded.
+					    tvStatus.setText("Fetching database...");
+					    prefCheckTablesExists();
 
-		                do {
-							sleep(10);
-		                } while(!DatabaseOperations.isCompleted[0] &&
-				                !DatabaseOperations.isCompleted[1] &&
-				                !DatabaseOperations.isCompleted[2] &&
-				                !DatabaseOperations.isCompleted[3] &&
-				                !DatabaseOperations.isCompleted[4] &&
-				                !DatabaseOperations.isCompleted[5] &&
-				                !DatabaseOperations.isCompleted[6]);
-	                }
-                }
-                catch(InterruptedException e) {
-	                e.printStackTrace();
-                }
-                finally {
-                    Intent iProfile = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(iProfile);
-                }
-            }
-        };
-        timer.start();
+					    do {
+						    sleep(100);
+					    } while(!DatabaseOperations.isCompleted[0] &&
+							    !DatabaseOperations.isCompleted[1] &&
+							    !DatabaseOperations.isCompleted[2] &&
+							    !DatabaseOperations.isCompleted[3] &&
+							    !DatabaseOperations.isCompleted[4] &&
+							    !DatabaseOperations.isCompleted[5] &&
+							    !DatabaseOperations.isCompleted[6]);
+
+					    Editor editor = prefInitialisation.edit();
+					    editor.putBoolean("pref_initialised", true);
+					    editor.commit();
+
+					    Intent iProfile = new Intent(SplashActivity.this, MainActivity.class);
+					    startActivity(iProfile);
+				    }
+			    }
+			    catch(InterruptedException e) {
+				    Editor editor = prefInitialisation.edit();
+				    editor.putBoolean("pref_initialised", false);
+				    editor.commit();
+
+				    e.printStackTrace();
+			    }
+		    }
+	    };
+
+	    try {
+		    if(!isInitialised) {
+			    tvStatus.setText("Checking Internet connection...");
+			    Thread.sleep(500);
+			    isConnected = FileOperations.hasInternetConnection(this);
+
+			    if(isConnected) {
+				    timer.start();
+			    }
+			    else {
+				    //TODO: App isn't connected to the Internet, prompt for retry or close.
+				    finish();
+			    }
+		    }
+		    else {
+			    tvStatus.setText("Completed!");
+			    Thread.sleep(1500);
+
+			    Intent iProfile = new Intent(SplashActivity.this, MainActivity.class);
+			    startActivity(iProfile);
+		    }
+	    }
+	    catch(InterruptedException e) {
+		    e.printStackTrace();
+	    }
     }
 
 	@Override
