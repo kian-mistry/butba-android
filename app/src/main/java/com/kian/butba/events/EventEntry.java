b548010a39e6c0d6c790b0e8074bb8108a2c9439
Kian Mistry <kian.mistry@gmail.com>
2017-03-05 22:05:11 +0000

    Users are now able to enter a maximum of 5 teams when filling in the entry form.

--- app/src/main/java/com/kian/butba/events/EventEntry.java
+++ app/src/main/java/com/kian/butba/events/EventEntry.java
@@ -7,12 +7,17 @@ import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
@@ -28,7 +33,7 @@ import java.util.HashMap;
  * Created by Kian Mistry on 17/01/17.
  */
 
-public class EventEntry extends AppCompatActivity {
+public class EventEntry extends AppCompatActivity implements OnItemSelectedListener {
 
 	public static String ENTRIES_EMAIL = "entries@butba.co.uk";
 
@@ -36,12 +41,14 @@ public class EventEntry extends AppCompatActivity {
 
 	private String eventName;
 	private int eventTeamSize;
+	private int numberOfTeams = 1;
 
-	private TextView eventEntryName;
-	private TextInputEditText eventEntryTeamName;
-	private GridLayout eventEntryAveragesGrid;
-	private RadioGroup eventEntryPaymentMethods;
-	private TextInputEditText eventEntryMoreInfo;
+	private TextView tvEventName;
+	private TextInputEditText etTeamName;
+	private AppCompatSpinner spNumberOfTeams;
+	private GridLayout glEntrants;
+	private RadioGroup rgPaymentMethods;
+	private TextInputEditText etMoreInfo;
 
 	private HashMap<String, String> bowlersNames;
 	private HashMap<String, Integer> bowlersAverages;
@@ -65,17 +72,22 @@ public class EventEntry extends AppCompatActivity {
 		eventTeamSize = intent.getIntExtra("EVENT_TEAM_SIZE", 0);
 
 		//Initialise views.
-		eventEntryName = (TextView) findViewById(R.id.event_entry_name);
-		eventEntryName.setText(eventName);
+		tvEventName = (TextView) findViewById(R.id.event_entry_name);
+		tvEventName.setText(eventName);
 
-		eventEntryTeamName = (TextInputEditText) findViewById(R.id.event_entry_team_name);
+		etTeamName = (TextInputEditText) findViewById(R.id.event_entry_team_name);
+		
+		spNumberOfTeams = (AppCompatSpinner) findViewById(R.id.event_entry_number_of_teams);
+		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.number_of_teams, android.R.layout.simple_spinner_item);
+		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spNumberOfTeams.setAdapter(spinnerAdapter);
+		spNumberOfTeams.setOnItemSelectedListener(this);
 
-		eventEntryAveragesGrid = (GridLayout) findViewById(R.id.event_entry_averages_grid);
-		setupAveragesView(eventTeamSize);
+		glEntrants = (GridLayout) findViewById(R.id.event_entry_averages_grid);
 
-		eventEntryPaymentMethods = (RadioGroup) findViewById(R.id.event_entry_payment_methods);
+		rgPaymentMethods = (RadioGroup) findViewById(R.id.event_entry_payment_methods);
 
-		eventEntryMoreInfo = (TextInputEditText) findViewById(R.id.event_entry_more_info);
+		etMoreInfo = (TextInputEditText) findViewById(R.id.event_entry_more_info);
 	}
 
 	@Override
@@ -99,8 +111,8 @@ public class EventEntry extends AppCompatActivity {
 				onBackPressed();
 				break;
 			case R.id.btn_event_entry_submit:
-				obtainTextFieldEntries();
-				submitEntry();
+				obtainTextFieldEntries(eventTeamSize, numberOfTeams);
+				submitEntry(eventTeamSize, numberOfTeams);
 				break;
 			default:
 				break;
@@ -113,105 +125,126 @@ public class EventEntry extends AppCompatActivity {
 	 * Populate the entry form with fields asking for the bowler's name and average depending on the team size for the event.
 	 *
 	 * @param teamSize The size of each team for the event.
+	 * @param numberOfTeams The number of teams the user wants to enter.
 	 */
-	private void setupAveragesView(int teamSize) {
+	private void setupEntrantsView(int teamSize, int numberOfTeams) {
 		if(teamSize > 0) {
-			for(int i = 0; i < teamSize; i++) {
-				//Create a linear layout for each row of the grid layout.
-				LinearLayout linearLayout = new LinearLayout(this);
-				linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-				linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-				//Create text input layout.
-				TextInputLayout textInputLayoutBowler = new TextInputLayout(this);
-				textInputLayoutBowler.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 3));
-
-				//Create edit text for bowler's name.
-				//TODO: Find a way to generate unique IDs for views.
-				TextInputEditText editTextBowler = new TextInputEditText(this);
-				editTextBowler.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-				editTextBowler.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
-				editTextBowler.setHint("Bowler " + (i + 1));
-
-				//Add to text input layout.
-				textInputLayoutBowler.addView(editTextBowler);
-
-				//Add to linear layout.
-				linearLayout.addView(textInputLayoutBowler);
-
-				//Create text input layout.
-				TextInputLayout textInputLayoutBowlerAverage = new TextInputLayout(this);
-				textInputLayoutBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
-
-				//Create edit text for bowler's name.
-				TextInputEditText editTextBowlerAverage = new TextInputEditText(this);
-				editTextBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-				editTextBowlerAverage.setInputType(InputType.TYPE_CLASS_NUMBER);
-				editTextBowlerAverage.setHint("Average");
-
-				//Add to text input layout.
-				textInputLayoutBowlerAverage.addView(editTextBowlerAverage);
-
-				//Add to linear layout.
-				linearLayout.addView(textInputLayoutBowlerAverage);
-
-				//Add linear layout to grid layout.
-				eventEntryAveragesGrid.addView(linearLayout);
+			
+			//Remove all child views from the grid layout, before redrawing the views.
+			glEntrants.removeAllViews();
+			
+			for(int i = 0; i < numberOfTeams; i++) {
+				for(int j = 1; j <= teamSize; j++) {
+					//Create a linear layout for each row of the grid layout.
+					LinearLayout linearLayout = new LinearLayout(this);
+					linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+					linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+					
+					//Create text input layout.
+					TextInputLayout textInputLayoutBowler = new TextInputLayout(this);
+					textInputLayoutBowler.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 3));
+					
+					//Create edit text for bowler's name.
+					TextInputEditText editTextBowler = new TextInputEditText(this);
+					editTextBowler.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+					editTextBowler.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+					editTextBowler.setHint("Bowler " + ((i * teamSize) + j));
+					
+					//Add to text input layout.
+					textInputLayoutBowler.addView(editTextBowler);
+					
+					//Add to linear layout.
+					linearLayout.addView(textInputLayoutBowler);
+					
+					//Create text input layout.
+					TextInputLayout textInputLayoutBowlerAverage = new TextInputLayout(this);
+					textInputLayoutBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
+					
+					//Create edit text for bowler's average.
+					TextInputEditText editTextBowlerAverage = new TextInputEditText(this);
+					editTextBowlerAverage.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+					editTextBowlerAverage.setInputType(InputType.TYPE_CLASS_NUMBER);
+					editTextBowlerAverage.setHint("Average");
+					
+					//Add to text input layout.
+					textInputLayoutBowlerAverage.addView(editTextBowlerAverage);
+					
+					//Add to linear layout.
+					linearLayout.addView(textInputLayoutBowlerAverage);
+					
+					//Add linear layout to grid layout.
+					glEntrants.addView(linearLayout);
+				}
 			}
 		}
 	}
 
 	/**
 	 * Obtain entries from each text field on the form.
+	 *
+	 * @param teamSize The size of each team for the event.
+	 * @param numberOfTeams The number of teams the user wants to enter.
 	 */
-	private void obtainTextFieldEntries() {
+	private void obtainTextFieldEntries(int teamSize, int numberOfTeams) {
 		bowlersNames = new HashMap<>();
 		bowlersAverages = new HashMap<>();
 
-		for(int i = 0; i < eventTeamSize; i++) {
-			LinearLayout linearLayout = (LinearLayout) eventEntryAveragesGrid.getChildAt(i);
-
-			//Bowler's Name
-			TextInputLayout textInputLayoutBowler = (TextInputLayout) linearLayout.getChildAt(0);
-			FrameLayout frameLayout = (FrameLayout) textInputLayoutBowler.getChildAt(0);
-			TextInputEditText editTextBowler = (TextInputEditText) frameLayout.getChildAt(0);
-			String name = (!editTextBowler.getText().toString().equals("")) ? editTextBowler.getText().toString() : null;
-			bowlersNames.put("bowler_" + (i + 1), name);
-
-			//Bowler's Average
-			TextInputLayout textInputLayoutBowlerAverage = (TextInputLayout) linearLayout.getChildAt(1);
-			frameLayout = (FrameLayout) textInputLayoutBowlerAverage.getChildAt(0);
-			TextInputEditText editTextBowlerAverage = (TextInputEditText) frameLayout.getChildAt(0);
-			Integer average = (!editTextBowlerAverage.getText().toString().equals("")) ? Integer.parseInt(editTextBowlerAverage.getText().toString()) : null;
-
-			bowlersAverages.put("bowler_" + (i + 1), average);
+		for(int i = 0; i < numberOfTeams; i++) {
+			for(int j = 1; j <= teamSize; j++) {
+				LinearLayout linearLayout = (LinearLayout) glEntrants.getChildAt((i * teamSize) + (j - 1));
+				
+				//Bowler's Name
+				TextInputLayout textInputLayoutBowler = (TextInputLayout) linearLayout.getChildAt(0);
+				FrameLayout frameLayout = (FrameLayout) textInputLayoutBowler.getChildAt(0);
+				TextInputEditText editTextBowler = (TextInputEditText) frameLayout.getChildAt(0);
+				String name = (!editTextBowler.getText().toString().equals("")) ? editTextBowler.getText().toString() : null;
+				bowlersNames.put("bowler_" + ((i * teamSize) + j), name);
+				
+				//Bowler's Average
+				TextInputLayout textInputLayoutBowlerAverage = (TextInputLayout) linearLayout.getChildAt(1);
+				frameLayout = (FrameLayout) textInputLayoutBowlerAverage.getChildAt(0);
+				TextInputEditText editTextBowlerAverage = (TextInputEditText) frameLayout.getChildAt(0);
+				Integer average = (!editTextBowlerAverage.getText().toString().equals("")) ? Integer.parseInt(editTextBowlerAverage.getText().toString()) : null;
+				
+				bowlersAverages.put("bowler_" + ((i * teamSize) + j), average);
+			}
 		}
 	}
 
 	/**
 	 * Packages the data obtained from the text fields to send to an email intent.
+	 *
+	 * @param teamSize The size of each team for the event.
+	 * @param numberOfTeams The number of teams the user wants to enter.
 	 */
-	private void submitEntry() {
+	private void submitEntry(int teamSize, int numberOfTeams) {
 		//Construct body of message.
 
 		//Team Name.
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("Club/Team Name: ");
-		stringBuilder.append(eventEntryTeamName.getText().toString() + "\n\n");
+		stringBuilder.append(etTeamName.getText().toString() + "\n\n");
 
 		//Bowlers within team.
 		stringBuilder.append("Bowlers: \n");
-		for(int i = 0; i < eventTeamSize; i++) {
-			String name = bowlersNames.get("bowler_" + (i + 1));
-			Integer average = bowlersAverages.get("bowler_" + (i + 1));
-
-			if(name != null) {
-				stringBuilder.append(name + " (" + average + ") \n");
+		for(int i = 0; i < numberOfTeams; i++) {
+			stringBuilder.append("Team " + (i + 1) + ": \n");
+			for(int j = 1; j <= teamSize; j++) {
+				String name = bowlersNames.get("bowler_" + ((i * teamSize) + j));
+				Integer average = bowlersAverages.get("bowler_" + ((i * teamSize) + j));
+				
+				if(name != null) {
+					if(average != null) {
+						stringBuilder.append(name + " (" + average + ") \n");
+					} else {
+						stringBuilder.append(name + " \n");
+					}
+				}
 			}
 		}
 
 		//Payment Method.
-		int checkedRadioButtonId = eventEntryPaymentMethods.getCheckedRadioButtonId();
+		int checkedRadioButtonId = rgPaymentMethods.getCheckedRadioButtonId();
 		stringBuilder.append("\nPayment Method: ");
 
 		if(checkedRadioButtonId != -1) {
@@ -223,9 +256,12 @@ public class EventEntry extends AppCompatActivity {
 			//None of the radio buttons are checked.
 			stringBuilder.append("Not specified. \n\n");
 		}
-
-		stringBuilder.append("More Information: \n");
-		stringBuilder.append(eventEntryMoreInfo.getText().toString() + "\n\n");
+		
+		String moreInfo = etMoreInfo.getText().toString();
+		if(!moreInfo.equals("")) {
+			stringBuilder.append("More Information: \n");
+			stringBuilder.append(etMoreInfo.getText().toString() + "\n\n");
+		}
 
 		//Create a new email intent.
 		Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
@@ -236,4 +272,13 @@ public class EventEntry extends AppCompatActivity {
 		emailIntent.setData(Uri.parse(uriBuilder.toString()));
 		startActivity(emailIntent);
 	}
+	
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		numberOfTeams = Integer.parseInt(parent.getItemAtPosition(position).toString());
+		setupEntrantsView(eventTeamSize, numberOfTeams);
+	}
+	
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {}
 }
